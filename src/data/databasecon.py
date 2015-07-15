11e94bae15277fb9cc15e0e499cbb42caceb8093
Yannik Hodel <yannik.hodel@gmail.com>
2015-07-15 10:27:48 +0200

    Added database-handling and prepared handlers for db-connection

---
+++ src/data/databasecon.py
@@ -0,0 +1,77 @@
+import psycopg2
+import os
+import urlparse
+
+__is_connected = False
+
+def connect_to_db():
+    if "DB_NAME" not in os.environ and "DB_HOST" not in os.environ and "DB_USER" not in os.environ and "DB_PASS" not in os.environ:
+        pass
+    else:
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ["DATABASE_URL"])
+        try:
+            con = psycopg2.connect(
+                database=url.path[1:],
+                user=url.username,
+                password=url.password,
+                host=url.hostname,
+                port=url.port
+                )
+        except Exception, e:
+            print("Can't connect to db")
+
+
+
+
+def disconnect_from_db(db_con):
+    db_con.close()
+
+
+
+def search_image(image_name, db_con):
+    cur = db_con.cursor()
+    query = """select image_link from image where image_name=%s"""
+    cur.execute(query, (image_name))
+    result = cur.fetchone()
+    if result != None:
+        image_link = result[0]
+        return image_link
+    else:
+        return "Not found"
+
+
+
+def add_image(image_link, image_name, user_id, db_con, update):
+    cur = db_con.cursor()
+    query = """select * from image where image_name=%s"""
+    cur.execute(query, (image_name,))
+    result = cur.fetchone()
+    if result == None:
+        query = """insert into image values(%s, %s, %s)"""
+        cur.execute(query, (image_name, image_link, user_id))
+
+        db_con.commit()
+    else:
+        telegram.send_message(
+        update["message"]["chat"]["id"],
+        "There's already an image with this name in the database"
+        )
+
+
+
+def add_user(user_id, user_name, db_con, update):
+    cur = db_con.cursor()
+    query = """select * from user where user_id=%s"""
+    cur.execute(query, (str(user_id),))
+    result = cur.fetchone()
+    if result == None:
+        query = """insert into user values(%s, %s)"""
+        cur.execute(query, (user_id, user_name))
+
+        db_con.commit()
+    else:
+        telegram.send_message(
+        update["message"]["chat"]["id"],
+        "There's already a user with this id in the database"
+        )
--- src/server/handlers.py
+++ src/server/handlers.py
@@ -1,5 +1,7 @@
 from data import telegram
 from doc import filehandler
+from data import databasecon
+import psycopg2
 
 
 def handle_help(update):
@@ -11,12 +13,29 @@ def handle_help(update):
 
 
 def handle_send(update):
-    pass #SEND IMAGE
+    telegram.send_message(
+        update["message"]["chat"]["id"],
+        "I'd send the requested image now.. If I could!"
+        )
+    #SEND IMAGE
 
 
 
-def handle_add(update):
+def handle_add_image(update):
+    #con = databasecon.connect_to_db()
+    name = "testname, please ignore" #Parse from text
+    link = "testlink, please ignore" #Parse from text
+    #databasecon.add_image(link, name, update["message"]["from"]["id"],con, update)
+    #databasecon.disconnect_from_db(con)
     pass #ADD IMAGE TO DB
+
+
+
+def handle_add_user(update):
+    #con = databasecon.connect_to_db()
+    #databasecon.add_user(update["message"]["from"]["id"],update["message"]["from"]["username"],con, update) #Need to parse id and name from text!
+    #databasecon.disconnect_from_db(con)
+    pass #ADD USER TO DB
 
 
 
@@ -44,7 +63,8 @@ def handle_fuckyou(update):
 __handlers = {
     "help": handle_help,
     "about": handle_about,
-    "add": handle_add,
+    "add": handle_add_image,
+    "adduser": handle_add_user,
     "fuckyou": handle_fuckyou
 }
 
