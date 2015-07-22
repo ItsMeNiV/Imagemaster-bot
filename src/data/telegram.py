8ff87ce3d5e2d3781b7bb0e02496c03437250c75
Yannik Hodel <yannik.hodel@gmail.com>
2015-07-22 15:28:12 +0200

    No Webm support yet. Because of problems on the serverside

--- src/data/telegram.py
+++ src/data/telegram.py
@@ -73,32 +73,36 @@ def send_photo(chat_id, image_link, image_name):
         data = {'chat_id': chat_id}
         response = requests.post(url, params=data, files=files)
     elif file_extension == "webm":
-        print("Iz a webm")
-        url = __url.format(__apikey, "sendDocument")
-        sendname = str("send.{0}").format(file_extension)
-        req = urllib.request.Request(image_link, headers={'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'})
-        print("Now downloading...")
-        try:
-            f = open(sendname, 'wb')
-            f.write(urllib.request.urlopen(req).read())
-            f.close()
-            print("done")
-        except Exception as e:
-            send_message(
+        #print("Iz a webm")
+        #url = __url.format(__apikey, "sendDocument")
+        #sendname = str("send.{0}").format(file_extension)
+        #req = urllib.request.Request(image_link, headers={'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'})
+        #print("Now downloading...")
+        #try:
+        #    f = open(sendname, 'wb')
+        #    f.write(urllib.request.urlopen(req).read())
+        #    f.close()
+        #    print("done")
+        #except Exception as e:
+        #    send_message(
+        #    chat_id,
+        #    "Failed to download webm"
+        #    )
+        #webm = open(sendname, 'rb')
+        #data = {'chat_id': chat_id}
+        #files = {'document': webm}
+        #print("Now uploading...")
+        #try:
+        #    response = requests.post(url, params=data, files=files)
+        #except Exception as e:
+        #    send_message(
+        #    chat_id,
+        #    "Failed to upload webm"
+        #    )
+        #print("done")
+        send_message(
             chat_id,
-            "Failed to download webm"
+            "Webm's are not supported yet. Sry"
             )
-        webm = open(sendname, 'rb')
-        data = {'chat_id': chat_id}
-        files = {'document': webm}
-        print("Now uploading...")
-        try:
-            response = requests.post(url, params=data, files=files)
-        except Exception as e:
-            send_message(
-            chat_id,
-            "Failed to upload webm"
-            )
-        print("done")
     else:
         pass
